93f8954e66cb56ea3a6b499f680eb23a874944d8
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-12-08 20:29:20 +0100

    Add $

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,9 +1,10 @@
 import java.util.stream.IntStream
 
-import org.apache.spark.ml.feature.{Interaction, VectorAssembler}
+import org.apache.spark.ml.feature.{ChiSqSelector, Interaction, VectorAssembler}
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+import org.spark_project.dmg.pmml.SparseArray
 
 object Runner {
 
@@ -11,6 +12,7 @@ object Runner {
 
     val conf = new SparkConf().setAppName("dtree-merge").setMaster("local")
     val ss = SparkSession.builder.config(conf).getOrCreate
+    import ss.implicits._
     var data = ss.read.option("inferSchema", "true").format("csv").load("A/biodeg.csv")
     var cols = data.columns
     var assembler = new VectorAssembler().setInputCols(cols.take(cols.size - 1)).setOutputCol("sparseFeatures")
@@ -18,20 +20,20 @@ object Runner {
 
     // vector assembler
     // pca / chi2
-    clfData.printSchema
-    clfData.show
-    clfData.select("_c0", "features").show(false)
-//    while (true) {}
+//    clfData.printSchema
+//    clfData.show
+//    clfData.select($"_c0" * 2, $"sparseFeatures").show(false)
 
-    val dat = Seq(
-      (7, Vectors.dense(0.0, 0.0, 18.0, 1.0), 1.0),
-      (8, Vectors.dense(0.0, 1.0, 12.0, 0.0), 0.0),
-      (9, Vectors.dense(1.0, 0.0, 15.0, 0.1), 0.0)
-    )
+    val selector = new ChiSqSelector().
+      setNumTopFeatures(2).
+      setFeaturesCol("sparseFeatures").
+      setLabelCol(cols.last).
+      setOutputCol("selectedFeatures")
 
-    val df = ss.createDataFrame(dat)
-    df.show()
+    val res = selector.fit(clfData).transform(clfData)
 
+    res.select("sparseFeatures", "selectedFeatures", cols.last).show()
+    res.printSchema()
 
   }
 
