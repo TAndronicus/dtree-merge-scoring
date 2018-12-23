9b5685b57c939b078bfc6abbd9feaae6959c58b7
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-12-23 20:30:53 +0100

    Config

--- src/main/scala/jb/Runner.scala
+++ src/main/scala/jb/Runner.scala
@@ -5,6 +5,8 @@ import jb.selector.Selector
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
 
+import jb.util.Conf._
+
 object Runner {
 
   def main(args: Array[String]): Unit = {
@@ -14,13 +16,13 @@ object Runner {
 
     val Array(trainingData, testData) = dat.randomSplit(Array(0.9, 0.1))
 
-    val dt = new DecisionTreeClassifier().setLabelCol("label").setFeaturesCol("features")
+    val dt = new DecisionTreeClassifier().setLabelCol(LABEL).setFeaturesCol(FEATURES)
     val model = dt.fit(trainingData)
     val prediction = model.transform(testData)
 
-    val evaluator = new BinaryClassificationEvaluator().setLabelCol("label").setRawPredictionCol("prediction")
+    val evaluator = new BinaryClassificationEvaluator().setLabelCol(LABEL).setRawPredictionCol(PREDICTION)
     print(evaluator.evaluate(prediction))
-    val multiEval = new MulticlassClassificationEvaluator().setLabelCol("label").setPredictionCol("prediction").setMetricName("accuracy")
+    val multiEval = new MulticlassClassificationEvaluator().setLabelCol(LABEL).setPredictionCol(PREDICTION).setMetricName("accuracy")
     print(multiEval.evaluate(prediction))
   }
 
--- src/main/scala/jb/util/Conf.scala
+++ src/main/scala/jb/util/Conf.scala
@@ -5,5 +5,6 @@ object Conf {
   val LABEL = "label"
   val SPARSE_FEATURES = "sparse_features"
   val FEATURES = "features"
+  val PREDICTION = "prediction"
 
 }
