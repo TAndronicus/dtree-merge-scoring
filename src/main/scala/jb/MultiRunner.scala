06139425fb84a681d8a3445e287028b2b1e01807
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-01-14 19:58:36 +0100

    Add reps

--- src/main/scala/jb/MultiRunner.scala
+++ src/main/scala/jb/MultiRunner.scala
@@ -6,9 +6,14 @@ object MultiRunner {
 
   def main(args: Array[String]): Unit = {
     SparkEmbedded.setLogWarn()
+    val reps = 20
     val runner = new Runner(5, 2, Array(2, 6, 10, 14))
-    val scores = runner.calculateMvIScores("A/so")
-    scores.foreach(i => print(i + "\n"))
+    var finalScores = Array(0D, 0D, 0D, 0D, 0D)
+    for (_ <- 0.until(reps)) {
+      val scores = runner.calculateMvIScores("A/so")
+      scores.indices.foreach(i => finalScores(i) += scores(i))
+    }
+    finalScores.map(_/reps).foreach(i => print(i + "\n"))
   }
 
 }
