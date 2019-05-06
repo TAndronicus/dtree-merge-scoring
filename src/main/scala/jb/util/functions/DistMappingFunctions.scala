f0c8822ab3cdf1be7e9d1d3636af52caa6d60702
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-05-06 15:31:21 +0200

    Fix mapping function

--- src/main/scala/jb/util/functions/DistMappingFunctions.scala
+++ src/main/scala/jb/util/functions/DistMappingFunctions.scala
@@ -3,9 +3,8 @@ package jb.util.functions
 object DistMappingFunctions {
 
   val simpleMapping: Double => Double = (dist: Double) => math.exp(exponentialCoefficient(dist))
-  val momentMappingFunction: (Double, Double) => Double = (distFromBorder: Double, distFromMoment: Double) =>.7 * simpleMapping(distFromBorder) +.3 * momentMapping(distFromBorder, distFromMoment)
-  val parametrizedMomentMappingFunction: Double => (Double, Double) => Double = (alpha: Double) => (distFromBorder: Double, distFromMoment: Double) => alpha * simpleMapping(distFromBorder) + (1 - alpha) * momentMapping(distFromBorder, distFromMoment)
-  private val momentMapping: (Double, Double) => Double = (distFromBorder: Double, distFromMoment: Double) => math.exp(exponentialCoefficient(distFromBorder) + exponentialCoefficient(distFromMoment))
+  val momentMappingFunction: (Double, Double) => Double = (distFromBorder: Double, distFromMoment: Double) =>.7 * simpleMapping(distFromBorder) +.3 * simpleMapping(distFromMoment)
+  val parametrizedMomentMappingFunction: Double => (Double, Double) => Double = (alpha: Double) => (distFromBorder: Double, distFromMoment: Double) => alpha * simpleMapping(distFromBorder) + (1 - alpha) * simpleMapping(distFromMoment)
 
   private def exponentialCoefficient(dist: Double): Double = {
     math.pow(10 * dist - 3, 2) / -4
