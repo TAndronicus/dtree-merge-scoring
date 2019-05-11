5b065f78d7842ec0304bc33d353a01b6d3347627
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-05-11 18:14:03 +0200

    Fix tests

--- src/test/scala/jb/model/dt/CombinedIntegratedDecisionTreeModelTest.scala
+++ src/test/scala/jb/model/dt/CombinedIntegratedDecisionTreeModelTest.scala
@@ -5,24 +5,6 @@ import org.scalatest.FunSuite
 
 class CombinedIntegratedDecisionTreeModelTest extends FunSuite {
 
-  test("point dist") {
-    val model = new CombinedIntegratedDecisionTreeModel(null, null, null, null)
-    val (x, y) = (2, 3.5)
-//    assert(model.pointDist(Array(x, y), Array(x + 3, y + 4)) == 5)
-  }
-
-  test("edge overlapping") {
-    val model = new CombinedIntegratedDecisionTreeModel(null, null, null, null)
-    val edge0 = Edge(Array(0, 0), Array(2, 0))
-    val edge1 = Edge(Array(0, 1), Array(0, 2))
-    val point = Array(1D, 0)
-
-//    assert(model.edgeOvelaps(edge0, point, 0))
-//    assert(!model.edgeOvelaps(edge0, point, 1))
-//    assert(!model.edgeOvelaps(edge1, point, 0))
-//    assert(!model.edgeOvelaps(edge1, point, 1))
-  }
-
   test("distance from point to edge") {
     val model = new CombinedIntegratedDecisionTreeModel(null, null, null, null)
     val point = Array(1D, 1)
@@ -36,12 +18,12 @@ class CombinedIntegratedDecisionTreeModelTest extends FunSuite {
   }
 
   test("min distance from point to edge") {
-    val model = new CombinedIntegratedDecisionTreeModel(null, null, null, null)
     val point = Array(1D, 1)
     val edge0 = Edge(Array(0, 0), Array(2, 0))
     val edge1 = Edge(Array(3, 0), Array(3, 5))
     val edge2 = Edge(Array(point(0) + 3, point(1) + 4), Array(point(0) + 3, point(1) + 6))
-//    assert(model.minDistUnsigned(Array(edge0, edge1, edge2), point) == 1)
+    val model = new CombinedIntegratedDecisionTreeModel(null, null, Array(Array(edge0, edge1, edge2)), null)
+    assert(model.minDistUnsigned(0, point) == 1)
   }
 
 }
--- src/test/scala/jb/model/dt/EdgeIntegratedDecisionTreeModelTest.scala
+++ src/test/scala/jb/model/dt/EdgeIntegratedDecisionTreeModelTest.scala
@@ -5,48 +5,43 @@ import org.scalatest.FunSuite
 
 class EdgeIntegratedDecisionTreeModelTest extends FunSuite {
 
-  test("point dist 2d") {
-//    val model = new EdgeIntegratedDecisionTreeModel(null, null, i => i)
-//    assert(model.pointDist(Array(1, 1), Array(4, 5)) == 5)
-  }
-
   test("dist unsigned x") {
     // given
     val p = Array(1.5, 1.5)
     val edge = Edge(Array(0, -.5), Array(2, -.5))
-//    val model = new EdgeIntegratedDecisionTreeModel(null, null, i => i)
+    val model = new EdgeIntegratedDecisionTreeModel(null, i => i, null)
 
     // when
-//    val dist = model.distUnsigned(edge, p)
+    val dist = model.distUnsigned(edge, p)
 
     // then
-//    assert(dist == 2)
+    assert(dist == 2)
   }
 
   test("dist unsigned y") {
     // given
     val p = Array(1.5, 1.5)
     val edge = Edge(Array(2, -.5), Array(2, 5))
-//    val model = new EdgeIntegratedDecisionTreeModel(null, null, i => i)
+    val model = new EdgeIntegratedDecisionTreeModel(null, i => i, null)
 
     // when
-//    val dist = model.distUnsigned(edge, p)
+    val dist = model.distUnsigned(edge, p)
 
     // then
-//    assert(dist == .5)
+    assert(dist == .5)
   }
 
   test("dist unsigned non-overlapping") {
     // given
     val p = Array(1.5, 1.5)
     val edge = Edge(Array(5.5, -5), Array(5.5, -1.5))
-//    val model = new EdgeIntegratedDecisionTreeModel(null, null, i => i)
+    val model = new EdgeIntegratedDecisionTreeModel(null, i => i, null)
 
     // when
-//    val dist = model.distUnsigned(edge, p)
+    val dist = model.distUnsigned(edge, p)
 
     // then
-//    assert(dist == 5)
+    assert(dist == 5)
   }
 
   test("min dist") {
@@ -57,13 +52,13 @@ class EdgeIntegratedDecisionTreeModelTest extends FunSuite {
       Edge(Array(2, -.5), Array(2, 5)),
       Edge(Array(0, -.5), Array(2, -.5))
     )
-//    val model = new EdgeIntegratedDecisionTreeModel(null, null, i => i)
+    val model = new EdgeIntegratedDecisionTreeModel(null, i => i, null)
 
     // when
-//    val dist = model.minDistUnsigned(edges, p)
+    val dist = model.minDistUnsigned(edges, p)
 
     // then
-//    assert(dist == .5)
+    assert(dist == .5)
   }
 
 }
