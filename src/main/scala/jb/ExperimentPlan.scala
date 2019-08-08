716f0837dbcf62140e9c26589c4b25c289fbb217
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-08-08 08:01:45 +0200

    Set up params

--- src/main/scala/jb/ExperimentPlan.scala
+++ src/main/scala/jb/ExperimentPlan.scala
@@ -7,11 +7,11 @@ object ExperimentPlan {
 
   def main(args: Array[String]): Unit = {
     SparkEmbedded.setLogError()
-    val nClassifs = Array(3)
+    val nClassifs = Array(3, 5, 7, 9)
     val nFeatures = 2
-    val alphas = Array(.7, .3, 0)
+    val alphas = Array(1, .7, .3, 0)
     val (beta1, beta2) = (.5, 0)
-    val (gamma1, gamma2) = (40, 20)
+    val (gamma1, gamma2) = (20, 5)
     for (nC <- nClassifs; alpha <- alphas) {
       val coeffs = Coefficients(alpha, beta1, beta2, gamma1, gamma2)
       MultiRunner.run(nC, nFeatures, coeffs, PreTraining())
